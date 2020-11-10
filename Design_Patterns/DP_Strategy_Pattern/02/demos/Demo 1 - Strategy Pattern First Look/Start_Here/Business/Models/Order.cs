25de7a8b38febd99861dc2b01d366f3068ae8117
Bruno Silva <brunosilvinosilva@gmail.com>
2020-11-10 13:04:54 +0000

    implemented strategy interface and refactored code to get tax for sweeden class

--- Design_Patterns/DP_Strategy_Pattern/02/demos/Demo 1 - Strategy Pattern First Look/Start_Here/Business/Models/Order.cs
+++ Design_Patterns/DP_Strategy_Pattern/02/demos/Demo 1 - Strategy Pattern First Look/Start_Here/Business/Models/Order.cs
@@ -25,39 +25,7 @@ namespace Strategy_Pattern_First_Look.Business.Models
 
             if(destination == "sweden")
             {
-                if (destination == ShippingDetails.OriginCountry.ToLowerInvariant())
-                {
-                    return TotalPrice * 0.25m;
-                }
-
-                #region Tax per item
-                //if (destination == ShippingDetails.OriginCountry.ToLowerInvariant())
-                //{
-                //    decimal totalTax = 0m;
-                //    foreach (var item in LineItems)
-                //    {
-                //        switch (item.Key.ItemType)
-                //        {
-                //            case ItemType.Food:
-                //                totalTax += (item.Key.Price * 0.06m) * item.Value;
-                //                break;
-
-                //            case ItemType.Literature:
-                //                totalTax += (item.Key.Price * 0.08m) * item.Value;
-                //                break;
-
-                //            case ItemType.Service:
-                //            case ItemType.Hardware:
-                //                totalTax += (item.Key.Price * 0.25m) * item.Value;
-                //                break;
-                //        }
-                //    }
-
-                //    return totalTax;
-                //}
-                #endregion
-
-                return 0;
+               
             }
 
             if (destination == "us")
---
+++ Design_Patterns/DP_Strategy_Pattern/02/demos/Demo 1 - Strategy Pattern First Look/Start_Here/Business/Strategies/SalesTax/SweedenSalesTaxStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.SalesTax
+{
+    public class SweedenSalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            var origin = order.ShippingDetails.OriginCountry.ToLowerInvariant();
+
+            if (destination == origin)
+            {
+                return order.TotalPrice * 0.25m;
+            }
+            return 0;
+        }
+    }
+}
