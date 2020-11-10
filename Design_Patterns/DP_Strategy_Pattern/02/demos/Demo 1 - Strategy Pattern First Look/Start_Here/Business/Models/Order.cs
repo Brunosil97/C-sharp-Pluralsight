dbedb47af3d47ce07fdcbd1d1b84807912b14f11
Bruno Silva <brunosilvinosilva@gmail.com>
2020-11-10 13:26:24 +0000

    pass interface into method

--- Design_Patterns/DP_Strategy_Pattern/02/demos/Demo 1 - Strategy Pattern First Look/Start_Here/Business/Models/Order.cs
+++ Design_Patterns/DP_Strategy_Pattern/02/demos/Demo 1 - Strategy Pattern First Look/Start_Here/Business/Models/Order.cs
@@ -22,11 +22,12 @@ namespace Strategy_Pattern_First_Look.Business.Models
 
         public ISalesTaxStrategy SalesTaxStrategy { get; set; }
 
-        public decimal GetTax()
+        public decimal GetTax(ISalesTaxStrategy salesTaxStrategy = default)
         {
+            var strategy = salesTaxStrategy ?? SalesTaxStrategy;
             if (SalesTaxStrategy == null) { return 0m; }
 
-            return SalesTaxStrategy.GetTaxFor(this);
+            return strategy.GetTaxFor(this);
         }
 
     }
